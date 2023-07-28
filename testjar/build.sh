859d31a4f795b82c4548eeec705d04cee4807fd1
fe1w0 <fe1w0@qq.com>
2023-07-29 03:11:12 +0800

    2023/07/29: finish 0.0.1 version.

--- testjar/build.sh
+++ testjar/build.sh
@@ -1,5 +1,6 @@
 BASE_DIR=/home/liuxr/Project/SoftwareAnalysis/DataSet/testjar
-BASE_DIR=/Users/fe1w0/Project/SoftWareAnalysis/DataSet/testjar
+#BASE_DIR=/Users/fe1w0/Project/SoftWareAnalysis/DataSet/testjar
+BASE_DIR=/home/fe1w0/SoftwareAnalysis/DataSet/testjar
 cd $BASE_DIR
 
 rm -rf classes/*
@@ -13,6 +14,7 @@ javac sources/*/*.java -d classes
 jar cvfm example.jar manifest.txt -C classes/ . 
 
 # copy exmaple.jar to FuzzChains
-cp ./example.jar /Users/fe1w0/Project/SoftWareAnalysis/Dynamic/FuzzChains/DataSet/target/
+cp ./example.jar /home/fe1w0/SoftwareAnalysis/DynamicAnalysis/FuzzChains/DataSet/target/
+# cp ./example.jar /Users/fe1w0/Project/SoftWareAnalysis/Dynamic/FuzzChains/DataSet/target/
 
 echo "[+] Finish."

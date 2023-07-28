f172c050c82e7774dedf4351fd56d2fb8a07f2a2
fe1w0 <xzasliuxinrong@gmail.com>
2023-07-28 21:49:42 +0800

    2023/07/28

--- testjar/sources/demo/ExpOne.java
+++ testjar/sources/demo/ExpOne.java
@@ -9,11 +9,15 @@ public class ExpOne extends SafeOne  {
 
     SafeClass chainTwo;
 
+    Integer size = 30;
+
     @Override
     public void check(SafeClass safeTwo) throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
-        System.out.println("Chain One");
-        chainTwo = safeTwo;
-        chainTwo.test(chainTwo.name);
+        if (size >= 40) {
+            System.out.println("Chain One");
+            chainTwo = safeTwo;
+            chainTwo.test(chainTwo.name);
+        }
     }
 }
 
